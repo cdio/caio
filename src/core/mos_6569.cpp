1d84fd723ed9b28bb9cf40ecb1cff1817947dfa1
Claudio Castiglia <cdio@kellas.eu>
2025-04-25 20:01:57 +0200

    core/mos_6569: Use case range.

--- src/core/mos_6569.cpp
+++ src/core/mos_6569.cpp
@@ -593,19 +593,7 @@ size_t Mos6569::tick(const Clock& clk)
         aec_pin = c_access();
         break;
 
-    case 15: case 16: case 17:
-    case 18: case 19: case 20:
-    case 21: case 22: case 23:
-    case 24: case 25: case 26:
-    case 27: case 28: case 29:
-    case 30: case 31: case 32:
-    case 33: case 34: case 35:
-    case 36: case 37: case 38:
-    case 39: case 40: case 41:
-    case 42: case 43: case 44:
-    case 45: case 46: case 47:
-    case 48: case 49: case 50:
-    case 51: case 52: case 53:
+    case 15 ... 53:
         g_access();
         aec_pin = c_access();
         break;
