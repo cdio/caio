c9d3262bb8ebe6625815e8d5d68eedde7369242f
Claudio Castiglia <cdio@kellas.eu>
2025-06-22 14:39:54 +0200

    core/joystick: typo

--- src/core/joystick.hpp
+++ src/core/joystick.hpp
@@ -49,7 +49,7 @@ struct Port {
  * Get the offset of a port member given its name.
  * @param name Name of the port member.
  * @return The offset to the member within the port structure;
- * -1 if the name does not correespond to a port member.
+ * -1 if the name does not correspond to a port member.
  * @see Port
  */
 ssize_t port_name_to_offset(std::string_view name);
